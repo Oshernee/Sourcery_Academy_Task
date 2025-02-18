5307572be60cf3b1291a977887a9ae29841520b5
Oshernee <ignas.pau@gmail.com>
2025-02-19 00:51:31 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -19,23 +19,33 @@ public class Main {
         compressString(compress);
         decompressString(decompress);
 
+        // As we already have example data, we start off from a for loop to go through each string in the list
+        // Then we create a StringBuilder object as when making a lot of changes to a string, it is better to use a StringBuilder object as Strings are immutable
+        // And can get expensive when making a lot of changes, we then create a variable i to keep track of the current index
+        // We then start a while loop to go through the string, we get the current character at index i and store it in currentChar
+        // We then create a variable count to keep track of the number of times the current character appears and start another while loop
+        // Which checks if the current character is the same as the character before it, if it is we increment count and continue
+        // After the while loop ends, we append the current character and the count to the StringBuilder object
+        // We then set the current string in the list to the StringBuilder object
         for (String s : compress) {
             System.out.println("Compressed - " + s);
         }
 
+        // As we already have example data, we start off from a for loop to go through each string in the list
+        // We get the string and then we create a StringBuilder object, we then start a for loop to go through each character in the string
+        // We get the current character and the count, and then we go through a while loop to check if the current character is a digit
+        // If it is we multiply the count by 10 and add the current digit to it, we then create a char array and fill it with the current character count times
+        // We then append the char array to the StringBuilder object, and set the current string in the list to the StringBuilder object
         for (String s : decompress) {
             System.out.println("Decompressed - " + s);
         }
     }
 
-    // As we already have example data, we start off from a for loop to go through each string in the list
-    // Then we create a StringBuilder object as when making a lot of changes to a string, it is better to use a StringBuilder object as Strings are immutable
-    // And can get expensive when making a lot of changes, we then create a variable i to keep track of the current index
-    // We then start a while loop to go through the string, we get the current character at index i and store it in currentChar
-    // We then create a variable count to keep track of the number of times the current character appears and start another while loop
-    // Which checks if the current character is the same as the character before it, if it is we increment count and continue
-    // After the while loop ends, we append the current character and the count to the StringBuilder object
-    // We then set the current string in the list to the StringBuilder object
+    /// <summary>
+    /// Compresses a list of strings by counting the number of times a character appears in a row
+    /// </summary>
+    /// <param name="data">List of strings to compress</param>
+    /// <returns>Compressed list of strings</returns>
     public static void compressString(List<String> data) {
         for (int index = 0; index < data.size(); index++) {
             String s = data.get(index);
@@ -54,11 +64,11 @@ public class Main {
         }
     }
 
-    // As we already have example data, we start off from a for loop to go through each string in the list
-    // We get the string and then we create a StringBuilder object, we then start a for loop to go through each character in the string
-    // We get the current character and the count, and then we go through a while loop to check if the current character is a digit
-    // If it is we multiply the count by 10 and add the current digit to it, we then create a char array and fill it with the current character count times
-    // We then append the char array to the StringBuilder object, and set the current string in the list to the StringBuilder object
+    /// <summary>
+    /// Decompresses a list of strings by repeating a character a number of times
+    /// </summary>
+    /// <param name="data">List of strings to decompress</param>
+    /// <returns>Decompressed list of strings</returns>
     public static void decompressString(List<String> data) {
         for (int index = 0; index < data.size(); index++) {
             String s = data.get(index);
